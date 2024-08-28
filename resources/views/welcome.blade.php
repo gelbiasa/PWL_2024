62514e6246bdaf48cab664f68afaf025f51d76b2
gelbiasa <gelbifirmansyah12@gmail.com>
2024-08-28 09:37:40 +0700

    Modifikasi Tampilan Awal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,14 @@
-<IDOCTYPE html>
-    <html lang-"[{ str_replace('_', '-', app()->getLocale()) }]">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>PWL 2024</title>
-    </head>
-    <body>
-        <h2>Pemrograman Web Lamjut 2024</h2>
-        <h3>Semangat Belajar Laravel 10</h3>
-    </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PWL 2024</title>
+</head>
+<body>
+    <h2>Pemrograman Web Lanjut 2024</h2>
+    <h3>Semangat Belajar Laravel 10</h3>
+    <h3>M. Isroqi Gelby Firmansyah 3C</h3>
+</body>
 </html>
